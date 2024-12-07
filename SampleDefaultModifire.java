365df8f33261d3ad7221c1c0704e6935344800d8
Bipul Singh <146985681+Bipulsandesh@users.noreply.github.com>
2024-12-07 20:50:52 +0530

    Create SampleDefaultModifire.java

---
+++ SampleDefaultModifire.java
@@ -0,0 +1,13 @@
+package pack1;
+
+public class SampleDefaultModifier {
+
+	public static void main(String[] args) {
+		 A obj1 = new A();
+		 obj1.show();  // default method in  class A
+	     B obj =new B();
+	     obj.display();  // default method in class B
+	     
+	}
+
+}
